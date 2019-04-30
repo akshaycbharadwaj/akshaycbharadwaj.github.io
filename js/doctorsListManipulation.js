20824467d52c4b15f82cfa67d349328dea3064cb
akshaycbharadwaj <38265950+akshaycb@users.noreply.github.com>
2019-04-30 16:06:03 -0700

    updated

--- js/doctorsListManipulation.js
+++ js/doctorsListManipulation.js
@@ -1,5 +1,5 @@
 let row = '<th>List of Doctors in San Francisco</th>';
 for (let i = 0; i < doctorsDetailsList.length; i++) {
-  row += `<tr><td><a target="__blank" href="doctors_details.html?${i}">${doctorsDetailsList[i]["name"]} </a></td></tr>`;
+  row += `<tr><td><a target="__blank" href="html/doctors_details.html?${i}">${doctorsDetailsList[i]["name"]} </a></td></tr>`;
 }
 document.getElementById('doctors_list').innerHTML = row;
